a41c1dc11904fc45bce83c574d80659c2b4e7b11
piyush-0120 <piyushsharma1420@gmail.com>
2022-01-13 13:55:31 +0530

    HomeFragment: fixed bugs in getting user's location

--- app/src/main/java/com/example/fuelonwheelsapp/HomeFragment.java
+++ app/src/main/java/com/example/fuelonwheelsapp/HomeFragment.java
@@ -2,6 +2,8 @@ package com.example.fuelonwheelsapp;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -10,7 +12,11 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -49,6 +55,14 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
     private String mParam1;
     private String mParam2;
     private FOWViewModel viewModel;
+
+    private GoogleMap mMap;
+
+    //private ActivityMapsBinding binding;
+    private LocationManager lm;
+    private LocationListener ll;
+    private Geocoder gc;
+
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -71,6 +85,8 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
         return fragment;
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,13 +96,6 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
         }
     }
 
-    private GoogleMap mMap;
-
-    //private ActivityMapsBinding binding;
-    LocationManager lm;
-    LocationListener ll;
-    Geocoder gc;
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -94,12 +103,20 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
         {
             if(grantResults.length>1 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
             {
-                if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,ll);
+                if(ContextCompat.checkSelfPermission(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,ll);
                 }
+            }
+            else {
+                Toast.makeText(getActivity(), "You need to allow Location to continue", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             }
         }
     }
+
+
+
+
     void generate_location(Location location)
     {
         mMap.clear();
@@ -108,23 +125,25 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
         //Log.i("Info",location.toString());
 
 
+        if(getActivity() != null && isAdded()){
 
-        gc = new Geocoder(getActivity(), Locale.getDefault());
-        try {
-            List<Address> l = gc.getFromLocation(location.getLatitude(),location.getLongitude(),1);
-            if (l!=null && l.size()>0){
-                Log.i("Location info",l.get(0).toString());
-                String add="";
+            gc = new Geocoder(getActivity(), Locale.getDefault());
+            try {
+                List<Address> l = gc.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                if (l!=null && l.size()>0){
+                    Log.i("Location info",l.get(0).toString());
+                    String add="";
 
-                if(l.get(0).getAddressLine(0)!=null)
-                    add+=l.get(0).getAddressLine(0);
-                viewModel.setOrderLocation(add);
-                Toast.makeText(getActivity(), add, Toast.LENGTH_SHORT).show();
-                mMap.addMarker(new MarkerOptions().position(user).title(add).draggable(true));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(user,15));
+                    if(l.get(0).getAddressLine(0)!=null)
+                        add+=l.get(0).getAddressLine(0);
+                    viewModel.setOrderLocation(add);
+                    Toast.makeText(getActivity(), add, Toast.LENGTH_SHORT).show();
+                    mMap.addMarker(new MarkerOptions().position(user).title(add).draggable(true));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(user,15));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -133,13 +152,20 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
 
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 mMap = googleMap;
-                viewModel = new ViewModelProvider(requireActivity()).get(FOWViewModel.class);
+                viewModel = new ViewModelProvider(getActivity()).get(FOWViewModel.class);
                 //mMap.setOnMapLongClickListener((GoogleMap.OnMapLongClickListener) getActivity());
                 //mMap.setOnMarkerDragListener((GoogleMap.OnMarkerDragListener) getActivity());
                 mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
@@ -157,7 +183,7 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
                     public void onMarkerDragEnd(Marker marker) {
                         Log.i("End",marker.getPosition().toString());
                         LatLng dragLoc = marker.getPosition();
-                        System.out.println("In Marketr");
+                        System.out.println("In Marker");
                         try {
                             List<Address> l = gc.getFromLocation(dragLoc.latitude,dragLoc.longitude,1);
                             if (l!=null && l.size()>0){
@@ -176,14 +202,15 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
                     }
                 });
 
-                lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                lm = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
 
                 ll = new LocationListener() {
                     @Override
                     public void onLocationChanged(@NonNull Location location) {
                         generate_location(location);//live first location
-                        lm.removeUpdates(ll);
-                        lm=null;
+//                        lm.removeUpdates(ll);
+//                        lm=null;
+                        lm.removeUpdates(this);
                     }
 
                     @Override
@@ -201,17 +228,31 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMapLongClickLi
 
                     }
                 };
-                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    //requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+                    ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
                 } else {
-                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
-                    Location lKnown = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    generate_location(lKnown);
+                    if( !lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ) {
+                        new AlertDialog.Builder(requireContext())
+                                .setTitle("GPS not found")  // GPS not found
+                                .setMessage("Enable GPS") // Want to enable?
+                                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        requireActivity().startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                                    }
+                                })
+                                .setNegativeButton("No", null)
+                                .show();
+                    }
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, ll);
+                    Location lKnown = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if(lKnown != null){
+                        generate_location(lKnown);
+                    }
                 }
             }
         });
 
-        return view;
 
     }
 
